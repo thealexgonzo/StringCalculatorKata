c610f075106ac40210c5d3d2426f7b66bdd111d3
Alejandro González <65364812+thealexgonzo@users.noreply.github.com>
2025-06-28 15:06:31 +0100

    Add project files.

---
+++ StringCalculatorKata/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StringCalculatorKata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ StringCalculatorTests/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace StringCalculatorTests
+{
+    public class Class1
+    {
+
+    }
+}
